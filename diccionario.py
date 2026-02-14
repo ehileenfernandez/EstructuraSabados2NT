ced568fd3af381c9622c425fd89298cf85bb144a
Ehileen Fernandez <ehileenfernandez@gmail.com>
2026-02-14 11:48:38 -0500

    feat se crea diccionario estudiante

---
+++ diccionario.py
@@ -0,0 +1,13 @@
+#creando un diccionario
+#estudiante ={
+ #   "id":
+#}
+#poblando (llenar) un diccionario
+# estudiante["id"]=123456
+# estudiante["documentos"]="123456"
+# estudiante["telefono"]="1234567890"
+# estudiante["promedio"]=3.5
+# estudiante["correo"]="correo@gmail.com"
+# estudiante["semestre"]=3
+# estudiante["nombre"]="Mario Hugo"
+# estudiante["esBecado"]=True
---
+++ principal.py
@@ -0,0 +1,8 @@
+idEstudiante=123456
+documentosEstudiante="123456"
+telefonoEstudiante="1234567890"
+promedioEstudiante=3.5
+correoEstudiante="correo@gmail.com"
+semestreEstudiante=3
+nombreEstudiante="Mario Hugo"
+esBecado=True
