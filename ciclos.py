c509890639ec5110f3f2316473d59e5f21bbadf5
Ehileen Fernandez <ehileenfernandez@gmail.com>
2026-02-14 14:34:27 -0500

    feat ciclos

---
+++ ciclos.py
@@ -0,0 +1,17 @@
+#ciclo While
+contador=1
+while contador<10: 
+    print("hola")
+    contador=+1
+
+ 
+
+#ciclo FOR
+#for contador in range(1,11,2):
+   # print(contador)
+
+for contador in range(5):
+    print("hola")
+
+   # for _ in range(5):
+   # print("hola")
